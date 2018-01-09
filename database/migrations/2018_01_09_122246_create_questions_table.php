670c6be5046c39ef7475b24cc1947e2b3d7e4cb6
kolydart <kolydart@macbeth.gateweb.gr>
2018-01-09 15:14:22 +0200

    update Question. Add multiple_choise

--- database/migrations/2018_01_09_122246_create_questions_table.php
+++ database/migrations/2018_01_09_122246_create_questions_table.php
@@ -16,6 +16,7 @@ class CreateQuestionsTable extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->increments('id');
             $table->string('text');
+            $table->boolean('multiple_choise')->default(true);
             $table->timestamps();
         });
     }
