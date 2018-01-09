cf2d9097d379c848ddc5818da73314e5e0d60777
kolydart <kolydart@macbeth.gateweb.gr>
2018-01-09 13:23:03 +0200

    Survey model. convert sql insert to single line

---
+++ database/migrations/2018_01_09_111207_create_surveys_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSurveysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surveys', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('institution');
+            $table->string('subject');
+            $table->text('intro');
+            $table->text('notes');
+            $table->string('teacher')->default('Κολυδάς, Τάσος');
+            $table->dateTime('date_begin');
+            $table->dateTime('date_end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('surveys');
+    }
+}
