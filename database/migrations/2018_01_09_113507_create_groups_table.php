303fd704f1fe5cc86514f15114e6214a0e2c1347
kolydart <kolydart@macbeth.gateweb.gr>
2018-01-09 14:10:16 +0200

    fixed & tested Group model

--- database/migrations/2018_01_09_113507_create_groups_table.php
+++ database/migrations/2018_01_09_113507_create_groups_table.php
@@ -16,6 +16,7 @@ class CreateGroupsTable extends Migration
         Schema::create('groups', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->integer('survey_id')->unsigned();
             $table->foreign('survey_id')
                 ->references('id')->on('surveys')
                 ->onDelete('cascade');
