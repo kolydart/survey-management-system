58958c4db115d45131b3b74226385b833dec249a
kolydart <kolydart@macbeth.gateweb.gr>
2018-01-14 17:16:41 +0200

    add relationships in Question / Questionnaire

--- app/Question.php
+++ app/Question.php
@@ -2,12 +2,22 @@
 
 namespace App;
 
+use App\Answer;
+use App\Questionnaire;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
     public function surveys(){
     	return $this->belongsToMany(Survey::class);
+    }
+
+    public function questionnaire(){
+    	return $this->belongsToMany(Questionnaire::class);
+    }
+    
+    public function answers(){
+    	return $this->belongsToMany(Answer::class);
     }
     
 }
---
+++ app/Questionnaire.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use App\Question;
+use App\Survey;
+use Illuminate\Database\Eloquent\Model;
+
+class Questionnaire extends Model {
+	/**
+	 * @return mixed
+	 */
+	public function survey() {
+		return $this->belongsTo(Survey::class);
+	}
+
+	public function questions(){
+		return $this->belongsToMany(Question::class);
+	}
+	
+}
