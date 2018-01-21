7d48fd1a1c82c6badcfbe9bee8361abac38200ce
kolydart <kolydart@macbeth.gateweb.gr>
2018-01-21 19:44:02 +0200

    POINT OF NO RETURN

--- database/migrations/2018_01_09_122246_create_questions_table.php
+++ database/migrations/2018_01_09_122246_create_questions_table.php
@@ -16,6 +16,8 @@ class CreateQuestionsTable extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->increments('id');
             $table->string('text');
+            $table->string('template'); // temporary
+            $table->string('paper_id'); // temporary
             $table->timestamps();
             $table->softDeletes();
         });
