b7eb7f3e82c3210eeae8db6361150eb0ecfc3e7f
kolydart <kolydart@macbeth.gateweb.gr>
2018-01-30 23:32:40 +0200

    disable default css, js

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -11,6 +11,8 @@
 
     <!-- Bootstrap core CSS -->
     <link href="/css/bootstrap.min.css" rel="stylesheet">
+    {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous"> --}}
+    {{-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script> --}}
 
     <!-- Custom styles for this template -->
     <link href="/css/blog.css" rel="stylesheet">
