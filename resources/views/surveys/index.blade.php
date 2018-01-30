7eb6ab9c03d9f0db7667dd56d053dd5bae49d2dd
kolydart <kolydart@macbeth.gateweb.gr>
2018-01-30 23:13:16 +0200

    bootstrap 4.0

--- resources/views/surveys/index.blade.php
+++ resources/views/surveys/index.blade.php
@@ -1,7 +1,6 @@
 @extends('layout.master')
 
 @section('content')
-<div class="container">
 	@foreach ($surveys as $survey)
 		<div class="card col-xl-4 col-lg-6 col-sm-8 col-xs-12">
 			<div class="card-header">
@@ -9,15 +8,12 @@
 			</div>
 	        <div class="card-body">
 	            @if ($survey->notes)
-	            	
-	            @endif<p class="card-text">
-	                {{$survey->notes}}
-	            </p>
+		            <p class="card-text"> {{$survey->notes}} </p>
+	            @endif
 	        </div>
 	        <div class="card-footer">
 	            <a class="btn btn-secondary my-2" href="{{route('questionnaires.index')}}"> Show questionnaires </a>
 	        </div>
 		</div>
 	@endforeach
-</div>
 @endsection
