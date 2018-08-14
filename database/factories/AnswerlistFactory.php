22c1ff128088aac610b936b087695b26b377b69e
kolydart <kolydart@macbeth.gateweb.gr>
2018-08-14 08:18:17 +0300

    answerlist@type values lowercase

--- database/factories/AnswerlistFactory.php
+++ database/factories/AnswerlistFactory.php
@@ -3,6 +3,6 @@
 $factory->define(App\Answerlist::class, function (Faker\Generator $faker) {
     return [
         "title" => $faker->name,
-        "type" => collect(["Radio","Radio + Text","Checkbox","Checkbox + Text","Text",])->random(),
+        "type" => collect(["radio","radio + text","checkbox","checkbox + text","text",])->random(),
     ];
 });
