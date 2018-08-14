eaed77154d57eff840eb12d63d61a5d55cdf20d3
kolydart <kolydart@macbeth.gateweb.gr>
2018-08-14 14:08:17 +0300

    named routes public.xxxx

--- routes/gw.php
+++ routes/gw.php
@@ -2,8 +2,8 @@
 
 Route::group(['prefix' => 'public', 'as' => 'public.'], function () {
 
-	Route::get('/'        , 'CollectController@index');
-	Route::get('{survey}' , 'CollectController@show');
-	Route::get('create'   , 'CollectController@create');
-	Route::post('/'       , 'CollectController@store');
+	Route::get('/'                , 'CollectController@index')->name('index');
+	// Route::get('{survey}'      , 'CollectController@show')->name('show');
+	Route::get('/{survey}/create' , 'CollectController@create')->name('create');
+	Route::post('/'               , 'CollectController@store')->name('store');
 });
