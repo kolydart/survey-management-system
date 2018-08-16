8c53a020476c46660e3cf8c4abb76fbe043b4843
kolydart <kolydart@macbeth.gateweb.gr>
2018-08-16 09:46:17 +0300

    jquery use local min.js

--- resources/views/public/master.blade.php
+++ resources/views/public/master.blade.php
@@ -17,6 +17,8 @@
     <link href="https://netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
     {{-- font-awesome --}}
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <!-- jQuery -->
+    <script src="/js/jquery-1.12.4.min.js"></script>
 
     @yield('head','')
   </head>
@@ -36,8 +38,6 @@
       
     </div>
 
-    <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery.js"></script>
     <!-- Bootstrap JavaScript -->
     <script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
   </body>
