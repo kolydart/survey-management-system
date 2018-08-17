0f39e938c575b513c7c2cca42998d23091cce1a8
kolydart <kolydart@macbeth.gateweb.gr>
2018-08-17 14:06:56 +0300

    improve render. font-weight-bold|normal

--- resources/views/partials/renderQuestionnaire.blade.php
+++ resources/views/partials/renderQuestionnaire.blade.php
@@ -103,6 +103,34 @@
                                 >
                                 {{ $answer->title }}
                             </span>
+                            {{-- show / hide textarea on load/reload --}}
+                            <script>
+                                jQuery(document).ready(function($) {
+                                    function check(){
+                                        /** font-weight-normal|bold */
+                                        if ($('input#{{$item->question->id}}_{{$answer->id}}_select').prop('checked')) {
+                                            $('span#{{$item->question->id}}_{{$answer->id}}_text')
+                                                .removeClass('font-weight-normal')
+                                                .addClass('font-weight-bold');
+                                        }else{
+                                            $('span#{{$item->question->id}}_{{$answer->id}}_text')
+                                                .removeClass('font-weight-bold')
+                                                .addClass('font-weight-normal');
+                                        }
+
+                                    };
+                                    /** run on first load */
+                                    $('#q_{{$item->question->id}} input[name^="{{$item->question->id}}_id"]').ready(function(){
+                                        check();
+                                    })
+
+                                    /** run on every change */
+                                    $('#q_{{$item->question->id}} input[name^="{{$item->question->id}}_id"]').change(function(){
+                                        check();
+                                    });
+                                });
+                            </script> 
+
 
                         {{-- hide-if-text end --}}
                         @endif
@@ -138,19 +166,12 @@
                                                 .attr('required', true)
                                                 .attr('disabled', false)
                                                 .show(300);
-                                            $('label[for={{$item->question->id}}_{{$answer->id}}_select]')
-                                                .removeClass('font-weight-normal')
-                                                .addClass('font-weight-bold');
-                                        }
-                                        else {
+                                        }else {
                                             $('#{{$item->question->id}}_content_{{$answer->id}}')
                                                 .val('')
                                                 .attr('required', false)
                                                 .attr('disabled', true)
                                                 .hide(300);
-                                            $('label[for={{$item->question->id}}_{{$answer->id}}_select]')
-                                                .removeClass('font-weight-bold')
-                                                .addClass('font-weight-normal');
                                         }                
                                     };
                                     /** run on first load */
