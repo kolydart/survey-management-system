kolydart/survey-management-system

commit 2d8697243ed3083f52fd049d9b0c5b9c64e29b50

minor; just adding forgotten ?? '' in views & jQuery empty shell

resources/views/partials/renderQuestionnaire.blade.php
@@ -32,17 +32,17 @@
         {{-- question --}}
         <div 
             class="form-group"
-            id="q_{{$item->question->id}}"
+            id="q_{{$item->question->id ?? ''}}"
             >
 
             {{-- label --}}
             <label class="col-md-6 {{-- control-label --}}" 
-                for="{{str_plural($item->question->answerlist->type)}}" 
+                for="{{str_plural($item->question->answerlist->type ?? '')}}" 
                 >
                     {{-- text --}}
-                    {!! $item->order !!} {!! $item->question->title !!}
+                    {!! $item->order !!} {!! $item->question->title ?? '' !!}
                     {{-- info tooltip for checkbox --}}
-                    @if ($item->question->answerlist->type == 'checkbox' && Route::getCurrentRoute()->getActionMethod() == 'create')
+                    @if ($item->question->answerlist->type ?? '' == 'checkbox' && Route::getCurrentRoute()->getActionMethod() == 'create')
                         <i data-toggle="tooltip" data-placement="top" title="@lang('Μία ή περισσότερες απαντήσεις')" class="fa fa-info-circle text-info"></i>
                     @endif
             </label>

resources/views/admin/items/index.blade.php
@@ -64,9 +64,9 @@
                                 <td field-key='survey'>{{ $item->survey->title or '' }}</td>
                                 <td field-key='survey_id'>{{$item->survey->id}}</td>
                                 <td field-key='question'>{{ $item->question->title or '' }}</td>
-                                <td field-key='question_id'>{{$item->question->id}}</td>
-                                <td field-key='answerlist'>{{ $item->question->answerlist->title or '' }}</td>
-                                <td field-key='answerlist_id'>{{$item->question->answerlist->id}}</td>
+                                <td field-key='question_id'>{{$item->question->id ?? '' }}</td>
+                                <td field-key='answerlist'>{{ $item->question->answerlist->title ?? '' }}</td>
+                                <td field-key='answerlist_id'>{{$item->question->answerlist->id ?? '' }}</td>
                                 <td field-key='responses_count'>{{App\Response::whereIn('questionnaire_id',$item->survey->questionnaires->pluck('id'))->where('question_id',$item->question_id)->count()}}</td>
                                 <td field-key='label'>{{ Form::checkbox("label", 1, $item->label == 1 ? true : false, ["disabled"]) }}</td>
                                 @if( request('show_deleted') == 1 )

resources/views/partials/javascripts.blade.php
@@ -61,6 +61,17 @@
              return this.href == url;
         }).parentsUntil('.sidebar-menu > .treeview-menu').addClass('menu-open').css('display', 'block');
     });
+
+
+    /**
+     * custom jquery code
+     */
+    jQuery(document).ready(function($) {
+
+
+
+
+    });
 </script>
 
  

commit 63a1202836fb65fddd633485ef6001e005d51ed9

fix bug introduced in 2d86972

resources/views/partials/renderQuestionnaire.blade.php
@@ -42,7 +42,7 @@
                     {{-- text --}}
                     {!! $item->order !!} {!! $item->question->title ?? '' !!}
                     {{-- info tooltip for checkbox --}}
-                    @if ($item->question->answerlist->type ?? '' == 'checkbox' && Route::getCurrentRoute()->getActionMethod() == 'create')
+                    @if ($item->question->answerlist->type == 'checkbox' && Route::getCurrentRoute()->getActionMethod() == 'create')
                         <i data-toggle="tooltip" data-placement="top" title="@lang('Μία ή περισσότερες απαντήσεις')" class="fa fa-info-circle text-info"></i>
                     @endif
             </label>