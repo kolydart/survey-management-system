d4db6103d031d69c3608ef8eaf3990d36dd9de19
kolydart <kolydart@macbeth.gateweb.gr>
2021-01-03 20:39:05 +0200

    migration: add hidden field to answers

---
+++ database/migrations/2021_01_03_171314_add_hidden_to_answers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Answer;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddHiddenToAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        /** add field to schema */
+        Schema::table('answers', function (Blueprint $table) {
+          $table->boolean('hidden')->default(false);
+        });
+
+        /** create a single record */
+        Answer::create(['title'=>'hidden','hidden'=>true]);
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('answers', function (Blueprint $table) {
+          $table->dropColumn('hidden');            
+        });
+    }
+}
