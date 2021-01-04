ddf0a2e0e0d592307418b23e1b23211bfb643282
kolydart <kolydart@macbeth.gateweb.gr>
2021-01-04 16:21:49 +0200

    minor; factory improvements

--- database/factories/AnswerlistFactory.php
+++ database/factories/AnswerlistFactory.php
@@ -2,7 +2,7 @@
 
 $factory->define(App\Answerlist::class, function (Faker\Generator $faker) {
     return [
-        "title" => $faker->name,
+        "title" => $faker->sentence,
         "type" => collect(["radio","checkbox","text","number","range","color","date","time","datetime","email","url",])->random(),
     ];
 });
--- database/factories/SurveyFactory.php
+++ database/factories/SurveyFactory.php
@@ -2,12 +2,12 @@
 
 $factory->define(App\Survey::class, function (Faker\Generator $faker) {
     return [
-        "title" => $faker->name,
+        "title" => $faker->word,
         "alias" => $faker->word,
         "institution_id" => factory('App\Institution')->create(),
         "introduction" => $faker->name,
-        "javascript" => $faker->name,
-        "notes" => $faker->name,
+        "javascript" => "",
+        "notes" => $faker->sentence,
         "inform" => 0,
         "access" => collect(["public","invited","registered",])->random(),
         "completed" => 0,
