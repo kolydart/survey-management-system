7f4e5fe1a47bf4baaaaa3f80d69e0c1433c4778e
kolydart <kolydar@macbeth.opera.gateweb.gr>
2022-05-19 13:19:54 +0300

    minor; formatting

--- resources/views/admin/questionnaires/show.blade.php
+++ resources/views/admin/questionnaires/show.blade.php
@@ -101,7 +101,7 @@
                                     <a href="{{ route('admin.responses.edit',[$response->id]) }}" class="btn btn-xs btn-info">@lang('quickadmin.qa_edit')</a>
                                     @endcan
                                     @can('response_delete')
-{!! Form::open(array(
+                                    {!! Form::open(array(
                                         'style' => 'display: inline-block;',
                                         'method' => 'DELETE',
                                         'onsubmit' => "return confirm('".trans("quickadmin.qa_are_you_sure")."');",
--- resources/views/partials/answerChart.blade.php
+++ resources/views/partials/answerChart.blade.php
@@ -38,9 +38,14 @@
                         @foreach ($item->question->answerlist->answers as $answer)
                             @if (App\Response::whereIn('questionnaire_id',$item->survey->questionnaires->pluck('id'))->where('question_id',$item->question_id)->count() > 0)
                                 "{{round(
-                                    App\Response::whereIn('questionnaire_id',$item->survey->questionnaires->pluck('id'))->where('question_id',$item->question_id)->where('answer_id',$answer->id)->count()
+                                    App\Response::whereIn('questionnaire_id',$item->survey->questionnaires->pluck('id'))
+                                        ->where('question_id',$item->question_id)
+                                        ->where('answer_id',$answer->id)
+                                        ->count()
                                     /
-                                    App\Response::whereIn('questionnaire_id',$item->survey->questionnaires->pluck('id'))->where('question_id',$item->question_id)->count()
+                                    App\Response::whereIn('questionnaire_id',$item->survey->questionnaires->pluck('id'))
+                                        ->where('question_id',$item->question_id)
+                                        ->count()
                                     *100
                                     ,2
                                     )}}",                       
