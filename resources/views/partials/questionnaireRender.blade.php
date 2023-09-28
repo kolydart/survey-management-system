f2cd3834446a0c9b7834fc85487ef2bdcf69568d
kolydart <kolydart@macbeth.gateweb.gr>
2023-09-28 13:58:07 +0300

    avoid breaking sections on print

--- resources/views/partials/questionnaireRender.blade.php
+++ resources/views/partials/questionnaireRender.blade.php
@@ -34,7 +34,7 @@ admin.questionnaires.show
     {{-- items --}}
     @foreach ( $survey->items()->orderBy('order')->get() as $item)
         {{-- item --}}
-        <section class="form-group gw-item q_{{$item->question->id ?? ''}} row" id="i_{{$item->id ?? ''}}" style="page-break-inside:avoid;">
+        <section class="form-group gw-item q_{{$item->question->id ?? ''}} row avoid-break" id="i_{{$item->id ?? ''}}" style="page-break-inside:avoid;">
             {{-- label (question) --}}
             <label 
                 class="@if (\Route::currentRouteName() == 'admin.surveys.show' && !$item->label ) col-md-6 col-lg-8 col-lg-offset-2 @else col-xs-12 @endif {{-- control-label --}} gw-label @if ($item->label) bg-primary gw-item-label @endif ">
