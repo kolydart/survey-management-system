cfe6d84af04d3bf8111e28004c463302f22f4177
kolydart <kolydart@macbeth.gateweb.gr>
2023-09-28 13:46:14 +0300

    config(app.name)

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Survey',
+    'name' => env('APP_NAME', 'Survey'),
 
     /*
     |--------------------------------------------------------------------------
